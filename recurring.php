fd2b8c7d31cd7a34b1e895643c09ec18113bab2d
Richard Castera <richard.castera@gmail.com>
2017-10-24 11:37:22 -0400

    Update recurring.php

--- recurring.php
+++ recurring.php
@@ -34,7 +34,7 @@ $PayFlow->setCustomerState('NY');
 $PayFlow->setCustomerZip('10018');
 $PayFlow->setCustomerCountry('US');
 $PayFlow->setCustomerPhone('212-123-1234');
-$PayFlow->setCustomerEmail('richard.castera@gmail.com');
+$PayFlow->setCustomerEmail('email@gmail.com');
 $PayFlow->setPaymentComment('New Monthly Transaction');
 
 if($PayFlow->processTransaction()):
